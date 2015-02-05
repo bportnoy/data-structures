4f1a0f50a76456b7413a335dff1b73d3b1da8979
bportnoy <bradley.portnoy@gmail.com>
2015-02-04 20:50:17 -0800

    Complete LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,35 +1,44 @@
 var LinkedList = function(){
   var list = {};
-  var index = 0;
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value){
     //create new node
-    newNode = Node(value);
+    var newNode = Node(value);
     //point previous tail next to new node
     if (list.tail !== null) {//if there's a tail
-      list[list.tail].next = list[index]; // assign next pointer of previous tail to new tail
-      //prevTail.next = list[index];
+      list.tail.next = newNode; // assign next pointer of previous tail to new tail
     }
-    list.tail = list[index];
-    if (list.head !== null) {
-      list.head = list[index];
+    list.tail = newNode; // assign tail to new node
+    if (list.head === null) {
+      list.head = newNode; // if there's no head, assign head to the new node
     }
-    index++;
   };
 
   list.removeHead = function(){
     //if null yell at someone
+    if (list.head === null) console.log("off with your head!");
     //get node from list.head & assign to variable
+    var oldHead = list.head;
     //set head to node.next
+    list.head = list.head.next;
     //delete node
-    //return result
+    var result = oldHead.value;
+    delete oldHead;
+    return result;
   };
 
   list.contains = function(target){
+    //debugger;
     var result = false;
-    //get list head
+    var current = list.head;
+    while(current.next){
+      if (current.value === target) result = true;
+      current = current.next;
+    }
+    if (current.value === target) result = true;
+    return result;
     //check to see if value = target
     //if it is, return true
     //else return false;
