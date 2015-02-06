86437871c5a5200be36e8f2e65aa626b81562ae9
Bradley Portnoy <bradley.portnoy@gmail.com>
2015-02-06 11:22:44 -0800

    Psuedocode for hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,27 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //store item in tuple
+  //check index and see if there's already something there
+  //if there isn't, store the tuple
+  //if there is, store them all in a bucket
+  //check to see if array needs to be resized, if it does, run doubling function
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  //check what's at the index and see if it's a bucket or just a tuple (if the value is equivalent to what we're looking for)
+  //if it's a tuple, return the values
+  //if it's a bucket, iterate and then return
+  //perhaps it's not found, return undefined
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  //check what's at the index, see if it's a bucket or a tuple
+  //if tuple, delete what's at the index
+  //if bucket, iterate until you find it and then remove
+  //if it needs to be resized, run halving function
 };
 
 
