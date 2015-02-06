3e102a5adc9e4a35bd63410b79f05970562046b6
bportnoy <bradley.portnoy@gmail.com>
2015-02-05 20:44:09 -0800

    Complete set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,25 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if (!this._storage.hasOwnProperty(item)){
+    this._storage[item] = item;
+  }
 };
 
 setPrototype.contains = function(item){
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item){
+  if (this._storage.hasOwnProperty(item)){
+    delete this._storage[item];
+  }
 };
 
 /*
