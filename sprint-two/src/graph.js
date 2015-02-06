8f73ca33859317fe648cb820498b834082ea2cd7
Bradley Portnoy <bradley.portnoy@gmail.com>
2015-02-05 16:54:12 -0800

    Remove extraneous comments

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -4,8 +4,6 @@ var Graph = function(){
   // instantiate a graph with variable for value
   // an array for nodes which are connected
   this.allNodes = [];
-  // this.nodeValue = null;
-  // this.connectedTo = [];
 };
 
 var GraphNode = function(value) {
@@ -16,37 +14,11 @@ var GraphNode = function(value) {
 //
 
 Graph.prototype.addNode = function(value){
-  // if (this.nodeValue === null) {
-  //   this.nodeValue = value;
-  // } else {
-  //   var newNode = new Graph();
-  //   newNode.nodeValue = value;
-  //   newNode.connectedTo.push(this);
-  //   this.connectedTo.push(newNode);
-  // }
   var newNode = new GraphNode(value);
   this.allNodes.push(newNode);
 };
 
 Graph.prototype.contains = function(target){
-  // var result = false;
-  //var explored = [];
-  // if (this.nodeValue === target) {
-  //   return true;
-  // } else {
-  //     for (var i = 0; i < this.connectedTo.length; i++) {
-  //       this.connectedTo[i].contains(target);
-  //       //explored.push(this.connectedTo[i]);
-  //     }
-  //     return false;
-  // }
-  //////////////////
-  // if (explored.length === this.connectedTo.length) {
-  //   return false;
-  // traverse connectedTo
-  // if value of current node is equal to target
-  // return true
-  // else run contains on child nodes
   for (var i = 0; i < this.allNodes.length; i++){
     if (this.allNodes[i].nodeValue === target) return true;
   }
@@ -54,29 +26,6 @@ Graph.prototype.contains = function(target){
 };
 
 Graph.prototype.removeNode = function(target){
-  // traverse the graph
-  // if node value equals target
-  // dereference from any edges
-  // remove the node
-  // if (this.nodeValue === target) {
-  //   // dereference all connections/edges
-  //   // delete the object
-  //   if (this.connectedTo.length === 0) delete this;
-  //   for (var i = 0; i < this.connectedTo.length; i++) {
-  //     var index = this.connectedTo[i].indexOf(this);
-  //     delete this.connectedTo[i].connectedTo[index];
-  //   }
-  //   delete this;
-  // } else {
-  //     for (var i = 0; i < this.connectedTo.length; i++) {
-  //       this.removeNode(target);
-  //     }
-  //   }
-  // for (var i = 0; i < this.allNodes.length; i++) {
-  //   if (this.allNodes[i].nodeValue === target) {
-
-  //   }
-  // }
   for (var i = 0; i < this.allNodes.length; i++){
     if (this.allNodes[i].nodeValue === target){
       this.allNodes.splice(i,1);
